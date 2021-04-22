98ccd6ef3fc05a0efd53d21a74bffcbee808bd73
yashaswini295558 <82228751+yashaswini295558@users.noreply.github.com>
2021-04-22 22:05:38 +0530

    Update example1.py

--- list/example1.py
+++ list/example1.py
@@ -1,1 +1,11 @@
+Write a Python program to find the second smallest number in a list
 
+
+
+def find_len(list1):
+    length = len(list1)
+    list1.sort()
+    print("Second Smallest element is:", list1[1])
+  
+list1=[12, 45, 2, 41, 31, 10, 8, 6, 4]
+Largest = find_len(list1)

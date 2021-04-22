5400f886f8a0c08a74e257581d22ab1e9fa790d4
yashaswini295558 <82228751+yashaswini295558@users.noreply.github.com>
2021-04-22 22:11:51 +0530

    Update and rename example.py to example1.py

---
+++ set/example1.py
@@ -0,0 +1,8 @@
+Write a Python program to find maximum and the minimum value in a set
+
+
+seta = set([5, 10, 3, 15, 2, 20])
+#Find maximum value
+print(max(seta))
+#Find minimum value
+print(min(seta))
